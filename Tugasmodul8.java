5d1e9b4383834c1643286a58c9974c63e150a51b
NicholasAdi24 <102377198+NicholasAdi24@users.noreply.github.com>
2022-05-01 18:12:46 +0700

    Update Tugasmodul8.java

--- Tugasmodul8.java
+++ Tugasmodul8.java
@@ -1,1 +1,16 @@
 
+package tugasmodul8;
+
+
+public class Tugasmodul8 {
+
+    public static void main(String[] args) {
+        home hm = new home();
+        hm.setVisible(true);
+        hm.pack();
+        hm.setLocationRelativeTo(null);
+        hm.setDefaultCloseOperation(home.EXIT_ON_CLOSE);
+
+    }
+    
+}
